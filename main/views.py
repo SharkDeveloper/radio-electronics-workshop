317a47c3e90eeb924a1f8e432a58473a68682e6c
Valerian <pvalera@gmail.com>
2023-09-06 04:53:43 +0300

    Исправление шаблонов сайта(wget)

--- main/views.py
+++ main/views.py
@@ -8,9 +8,10 @@ def index(request):
     return render(request,"main/index.html")
 
 def about(request):
-    return render(request,'main/about.html')
+    return render(request,'main/my-site/about.html')
 
 def contact_us(request):
+    print(f"\ncontact_us \n {request}")
     form = OrderForm(request.POST)
     if request.method == "POST":
         if form.is_valid():
@@ -20,9 +21,10 @@ def contact_us(request):
         'form':form
     }
 
-    return render(request,"main/contact_us.html",context)
+    return render(request,"main/my-site/contact_us.html",context)
 
 def reviews(request):
+    print(f"\nreviews \n {request}")
     form = FeedbackForm(request.POST)
     last_three_feedback = Feedback.objects.all().order_by('-id')[:3]
     if request.method == "POST":
@@ -38,5 +40,5 @@ def reviews(request):
         'feedback2':last_three_feedback[1],
         'feedback3':last_three_feedback[0]
     }
-    return render(request,"main/reviews.html",context)
+    return render(request,"main/my-site/reviews.html",context)
 
